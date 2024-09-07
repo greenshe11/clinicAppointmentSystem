8cc177f0fbf3ff057d05aa7b8740a74007f1e9c2
Mykull Dodge <52579088+skidd104@users.noreply.github.com>
2024-09-07 20:19:40 +0800

    latest commit
    07/09/2024 8:19pm

--- app.py
+++ app.py
@@ -92,6 +92,26 @@ class App:
                 return render_template('schedule.html')
             except Exception as e:
                 return f"Error: {e}", 500
+            
+        @self.app.route('/history')
+        def history_home():
+            if util.no_user_logged_in(): #proceeds to schedule page if not logged in
+                return redirect('/home')
+            try:
+                return render_template('history.html')
+            except Exception as e:
+                return f"Error: {e}", 500
+            
+        @self.app.route('/about')
+        def about_home():
+            if util.no_user_logged_in(): #proceeds to schedule page if not logged in
+                return redirect('/home')
+            try:
+                return render_template('About.html')
+            except Exception as e:
+                return f"Error: {e}", 500
+            
+
 
     def create_api_routes(self):
         patient_routes(self, 'tblpatient')
